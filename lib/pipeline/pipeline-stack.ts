1519f3c935fdb158a8f46d7769069ee1422ff9cf
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-01-17 16:46:13 -0300

    first dev pipeline

---
+++ lib/pipeline/pipeline-stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cdk_pipeline from 'aws-cdk-lib/pipelines';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+interface PipelineStackProps extends cdk.StackProps {
+  branch: string;
+  awsAccount: string;
+  awsRegion: string;
+}
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: PipelineStackProps) {
+    super(scope, id, props);
+
+    const pipeline = new cdk_pipeline
+      .CodePipeline(
+        this, 'CognitoPipeLine'.concat(props.branch), {
+          pipelineName: 'CognitoPipeLine'.concat(props.branch),
+          dockerEnabledForSynth: true,
+          crossAccountKeys: true,
+          synth: new cdk_pipeline.ShellStep('Synth', {
+            input: cdk_pipeline.CodePipelineSource
+              .gitHub(`${process.env.CODE_SOURCE}`, props.branch),
+            commands: [
+              'npm ci',
+              'npm run build',
+              'npx cdk synth'
+            ]  
+          })
+      });
+
+    // add stage here
+  }
+}
