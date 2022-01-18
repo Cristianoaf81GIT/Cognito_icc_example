f8ac11edc1c0f49a2b1b5b4199309bf62ebd84b1
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-01-18 08:58:09 -0300

    update products function

--- lambda/productsFetchFunction.js
+++ lambda/productsFetchFunction.js
@@ -1,6 +1,7 @@
 const AWS = require("aws-sdk");
 const AWSXRay = require("aws-xray-sdk-core");
 const xRay = AWSXRay.captureAWS(require("aws-sdk"));
+const awsRegion = process.env.AWS_REGION;
 
 AWS.config.update({
   region: awsRegion,
