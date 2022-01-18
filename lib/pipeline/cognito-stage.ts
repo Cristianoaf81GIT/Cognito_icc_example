867fe6c9a8fe3f06f541d64cdb71b5988f297f13
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-01-18 08:33:10 -0300

    Api GW and products fetch func

--- lib/pipeline/cognito-stage.ts
+++ lib/pipeline/cognito-stage.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CognitoAppStack } from '../stack/cognitoApp-stack';
+import { ProductsFetchStack } from '../stack/productsFetch-stack';
 
 interface CognitoStageProps extends cdk.StackProps {
   branch: string;
@@ -8,5 +10,21 @@ interface CognitoStageProps extends cdk.StackProps {
 export class CognitoStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props: CognitoStageProps) {
     super(scope, id, props);
+
+    const productsFetchStack = new ProductsFetchStack(
+      this, 
+      'ProductsFetch'
+    );
+
+    const cognitoAppStack = new CognitoAppStack(
+      this,
+      'CognitoApp',
+      {
+        branch: props.branch,
+        productsFetchHandler: productsFetchStack.productsFetchHandler
+      }
+    );
+
+    cognitoAppStack.addDependency(productsFetchStack);
   }
 }
