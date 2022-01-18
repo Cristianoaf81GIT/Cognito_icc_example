c5396ddda3f4e02f59ef238fb4383cc33f4afe6b
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2022-01-18 09:41:50 -0300

    update

--- bin/cognito_icc.ts
+++ bin/cognito_icc.ts
@@ -11,11 +11,11 @@ const app = new cdk.App();
 
 new PipelineStack(app, 'CognitoDevPipelineStack', {
   branch: 'dev',
-  awsRegion: `${process.env.AWS_REGION}`,
-  awsAccount: `${process.env.AWS_ACCOUNT_DEV}`,
+  awsRegion: `us-east-1`,
+  awsAccount: `151783411801`,
   env: {
-    account: `${process.env.AWS_ACCOUNT_MAIN_ID}`,
-    region: `${process.env.AWS_REGION}`
+    account: `315153108667`,
+    region: `us-east-1`
   }
 });
 
